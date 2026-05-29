8aa8c0607b86d14e4f804355d6616769019500c7
zmaljefri-debug <zmaljefri@stu.kau.edu.sa>
2026-05-30 00:58:16 +0300

    Update feedback.php

--- feedback.php
+++ feedback.php
@@ -1,5 +1,8 @@
 <?php
 // Starts the session to access logged-in user information
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 session_start();
 
 // Includes the database connection file
