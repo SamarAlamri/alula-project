ed7e96804cbcbe2171bfb18474864036f99a0918
SamarAlamri <Samaralamri09@gmail.com>
2026-06-02 22:09:57 +0300

    added email confirmation to tour booking

--- pages/mytour.php
+++ pages/mytour.php
@@ -1,10 +1,22 @@
 <!-- Name: [Zain Aljifry], ID: [2107808], Section: [DAR], Date: [8 march] | Name: Samar Alamri, ID: 2206831, Section: DAR, Date: 8 march |Name: Talah Faloudah, ID: 2206666, Section: DAR, Date: 8 march -->
-
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+?>
 <?php
 include "../includes/auth.php";
 requireLogin();
 
 include "../includes/db.php"; 
+require '../phpmailer/PHPMailer.php';
+require '../phpmailer/SMTP.php';
+require '../phpmailer/Exception.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+$bookingMessage = "";
+$bookingError = "";
 
 $user_id = $_SESSION['user_id'];
 
@@ -21,7 +33,67 @@ if (isset($_GET['tour_id'])) {
         $insertSql = "INSERT INTO my_trips (user_id, tour_id) VALUES (?, ?)";
         $insertStmt = $conn->prepare($insertSql);
         $insertStmt->bind_param("ii", $user_id, $tour_id);
-        $insertStmt->execute();
+
+        if ($insertStmt->execute()) {
+
+            $userSql = "SELECT name, email FROM users WHERE id = ?";
+            $userStmt = $conn->prepare($userSql);
+            $userStmt->bind_param("i", $user_id);
+            $userStmt->execute();
+            $user = $userStmt->get_result()->fetch_assoc();
+
+            $tourSql = "SELECT title, duration, price FROM tours WHERE id = ?";
+            $tourStmt = $conn->prepare($tourSql);
+            $tourStmt->bind_param("i", $tour_id);
+            $tourStmt->execute();
+            $tourData = $tourStmt->get_result()->fetch_assoc();
+
+            try {
+                $mail = new PHPMailer(true);
+
+                $mail->isSMTP();
+                $mail->Host = 'smtp.gmail.com';
+                $mail->SMTPAuth = true;
+                $mail->Username = 'alula360project@gmail.com';
+                $mail->Password = 'qsmnagcqxgavgnmn';
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+                $mail->Port = 587;
+
+                $mail->setFrom('alula360project@gmail.com', 'AlUla 360');
+                $mail->addAddress($user['email'], $user['name']);
+
+                $mail->isHTML(true);
+                $mail->Subject = 'AlUla 360 Tour Booking Confirmation';
+
+                $mail->Body = "
+                    <div style='font-family: Arial, sans-serif; line-height: 1.7; color: #4a3a33;'>
+                        <h2 style='color:#7a3b1d;'>Tour Booking Confirmed</h2>
+
+                        <p>Dear <strong>{$user['name']}</strong>,</p>
+
+                        <p>Your tour booking has been successfully confirmed.</p>
+
+                        <p>
+                            <strong>Tour:</strong> {$tourData['title']}<br>
+                            <strong>Duration:</strong> {$tourData['duration']}<br>
+                            <strong>Price:</strong> {$tourData['price']} SAR
+                        </p>
+
+                        <p>You can view your itinerary anytime from the My Tour page.</p>
+
+                        <p>Thank you for choosing AlUla 360.</p>
+                    </div>
+                ";
+
+                $mail->send();
+
+                $bookingMessage = "Tour booked successfully. A confirmation email has been sent to your email.";
+            } catch (Exception $e) {
+                $bookingError = "Tour booked successfully, but the confirmation email could not be sent.";
+            }
+        }
+    } else {
+        $bookingMessage = "This tour is already in your My Tour page.";
     }
 }
 ?>
@@ -58,7 +130,15 @@ include "../includes/header.php";
     <!-- Main content area where the tour schedule is displayed -->
     <div id="main-content">
 
-        <?php
+        <?php if (!empty($bookingMessage)): ?>
+            <p class="success-message"><?php echo $bookingMessage; ?></p>
+        <?php endif; ?>
+
+        <?php if (!empty($bookingError)): ?>
+            <p class="error-message"><?php echo $bookingError; ?></p>
+        <?php endif; ?>
+                <?php
+
         $sql = "SELECT tours.*
                 FROM my_trips
                 JOIN tours ON my_trips.tour_id = tours.id
